6c89cf7d14ed222bdb69ffce7f15583a6c3e3fd6
unknown <mohamed.sayed14527@yahoo.com>
2022-09-06 17:46:42 +0200

    manage.py added

---
+++ manage.py
@@ -0,0 +1,44 @@
+from flask_restx import Api
+from flask import Flask
+from db import db
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+import os
+from user.router import user_ns
+from posts.router import post_ns
+from e_commerce.router import com
+
+
+
+app = Flask(__name__, static_url_path='', static_folder='')
+
+# CORS(app)
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+app.config['SECRET_KEY'] = 'mohamed sayed'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'databases/data.db')
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+JWTManager(app)
+
+authorization = {
+    'apikey' : {
+        'type' : 'apiKey',
+        'in' : 'header',
+        'name' : 'Authorization'
+    }
+}
+
+db.init_app(app)
+
+
+migrate = Migrate(app, db)
+
+api = Api(app, doc='/api', authorizations=authorization, description="This an api for important news")
+api.add_namespace(user_ns)
+api.add_namespace(post_ns)
+api.add_namespace(com)
